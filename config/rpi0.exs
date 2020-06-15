589630cc3e7ad075ca387915f29204e3c5e63b1f
corka149 <corka149@mailbox.org>
2020-06-15 15:34:27 +0200

    Cleaned config

--- config/rpi0.exs
+++ config/rpi0.exs
@@ -2,7 +2,5 @@ import Config
 
 config :pond_pump,
   power_in_pin: 18,
-  device_name: "pond_pump_149"
-
-config :pump_light,
+  device_name: "pond_pump_149",
   address: System.fetch_env!("PUMP_LIGHT_ADDRESS")
