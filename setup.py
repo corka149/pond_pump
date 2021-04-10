db532f6e450bd8452ce0ad7c9e5a7181b8f3c586
corka149 <corka149@mailbox.org>
2021-04-10 10:13:01 +0200

    Added boilerplate for deploying

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='pond_pump',
-    version='',
+    version='1.0.0',
     packages=['pond_pump', 'pond_pump.model', 'pond_pump.infrastructure'],
     package_dir={'': 'src'},
+    include_package_data=True,  # IMPORTANT for configs in combination with MANIFEST.in
     url='https://github.com/corka149/pond_pump',
     license='',
     author='corka',
