2e2b16f18cb815aa01b60c9fa3940a43200f6257
corka149 <corka149@mailbox.org>
2021-04-11 09:41:22 +0200

    Added pin factory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pond_pump',
-    version='1.0.2',
+    version='1.0.3',
     packages=['pond_pump', 'pond_pump.model', 'pond_pump.infrastructure'],
     package_dir={'': 'src'},
     include_package_data=True,  # IMPORTANT for configs in combination with MANIFEST.in
@@ -15,6 +15,7 @@ setup(
         'pydantic',
         'PyYAML',
         'aiohttp',
-        'gpiozero'
+        'gpiozero',
+        'RPi.GPIO'
     ]
 )
