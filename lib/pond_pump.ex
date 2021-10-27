cfa04f077ecfed0b27458adc677ca1f71881cdb1
corka149 <corka149@mailbox.org>
2021-10-27 22:47:12 +0200

    Switch to nerves

---
+++ lib/pond_pump.ex
@@ -0,0 +1,18 @@
+defmodule PondPump do
+  @moduledoc """
+  Documentation for PondPump.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PondPump.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
--- src/pond_pump/exception.py
+++
@@ -1,5 +0,0 @@
-""" Pump light specific exceptions. """
-
-
-class EndedTooEarlyException(BaseException):
-    """ Will be thrown when somehow the listener finishes what never should happen. """
