2b67b33d77ea8785e5bfed812f40fd49a5f72de2
corka149 <corka149@mailbox.org>
2021-10-30 08:36:30 +0200

    Create power check task

--- lib/pond_pump/power_check.ex
+++ lib/pond_pump/power_check.ex
@@ -1,15 +1,34 @@
 defmodule PondPump.PowerCheck do
-  use GenServer
+  use Task
 
-  @impl true
-  def init(power_pin) do
-    {:ok, gpio} = Circuits.GPIO.open(power_pin, :input)
+  def start_link(power_pin, notification_pin) do
+    power_gpio = setup_power(power_pin)
+    notification_gpio = setup_notification(notification_pin)
+
+    Task.start_link(__MODULE__, :run, [power_gpio, notification_gpio])
+  end
+
+  def run(power_gpio, notification_gpio) do
+    receive do
+      {:circuits_gpio, _pin, _timestamp, value} ->
+        :ok = Circuits.GPIO.write(notification_gpio, value)
+        run(power_gpio, notification_gpio)
+    end
+  end
+
+  defp setup_power(pin) do
+    {:ok, gpio} = Circuits.GPIO.open(pin, :input)
 
     :ok = Circuits.GPIO.set_interrupts(gpio, :both)
 
     # https://github.com/raspberrypilearning/physical-computing-guide/blob/master/pull_up_down.md
     :ok = Circuits.GPIO.set_pull_mode(gpio, :pulldown)
 
-    {:ok, gpio}
+    gpio
+  end
+
+  defp setup_notification(pin) do
+    {:ok, gpio} = Circuits.GPIO.open(pin, :output)
+    gpio
   end
 end
