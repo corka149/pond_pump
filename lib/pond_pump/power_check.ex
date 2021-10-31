c7a1e7be40e4bd7560c5890897dd174ff71612d9
corka149 <corka149@mailbox.org>
2021-10-31 14:08:39 +0100

    Improved logging

--- lib/pond_pump/power_check.ex
+++ lib/pond_pump/power_check.ex
@@ -1,13 +1,15 @@
 defmodule PondPump.PowerCheck do
   use Task, restart: :permanent
 
+  require Logger
+
   def start_link([power_pin, notification_pin]) do
     Task.start_link(__MODULE__, :run_init, [power_pin, notification_pin])
   end
 
   def run_init(power_pin, notification_pin) do
-    IO.puts("Start listing #{power_pin}")
-    IO.puts("Ready to write to #{notification_pin}")
+    Logger.info("Start listing #{power_pin}")
+    Logger.info("Ready to write to #{notification_pin}")
 
     power_gpio = setup_power(power_pin)
     notification_gpio = setup_notification(notification_pin)
@@ -18,9 +20,12 @@ defmodule PondPump.PowerCheck do
   def run(power_gpio, notification_gpio) do
     receive do
       {:circuits_gpio, _pin, _timestamp, value} ->
-        IO.puts("Value changed to #{value}")
+        Logger.info("Value changed to #{value}")
         :ok = Circuits.GPIO.write(notification_gpio, value)
         run(power_gpio, notification_gpio)
+
+      unknown_notification ->
+        Logger.warn("Unknown message #{unknown_notification}")
     end
   end
 
