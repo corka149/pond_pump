97000cd8233f6ea746b2dc9f7f00dbbc684ff474
corka149 <corka149@mailbox.org>
2021-12-04 09:03:11 +0100

    Clean code `transmit`

--- lib/pond_pump/radio_util.ex
+++ lib/pond_pump/radio_util.ex
@@ -1,14 +1,29 @@
 defmodule PondPump.RadioUtil do
   @short_delay 0.00045
   @long_delay 0.00090
-  @extended_delay 0.0096
+  # TODO check if this still necessary
+  # @extended_delay 0.0096
 
+  @doc """
+  Transmit a binary code via GPIO.
+
+  ## Examples
+
+      iex> RadioUtil.transmit([1, 0, 1, 0, 1], 17)
+  """
+  @spec transmit(list, non_neg_integer) :: :ok
   def transmit(code, gpio_pin) when is_list(code) do
     {:ok, gpio} = Circuits.GPIO.open(gpio_pin, :output)
+
     do_transmit(gpio, code)
 
-    :ok = Circuits.GPIO.write(gpio, 0)
-    Process.sleep(@extended_delay)
+    Circuits.GPIO.write(gpio, 0)
+  end
+
+  # ===== ===== PRIVATE ===== =====
+
+  defp do_transmit(_, []) do
+    :ok
   end
 
   defp do_transmit(gpio, [1 | tail]) do
