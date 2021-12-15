f0bdb9fab810bc9657e822e506f39bc09c1a8541
corka149 <corka149@mailbox.org>
2021-12-15 21:30:55 +0100

    Add BuildHelper

--- config/rpi0.exs
+++ config/rpi0.exs
@@ -1,11 +1,13 @@
 import Config
 
-defp extract_mode! do
-  case System.get_env("POND_PUMP_MODE") do
-    nil -> raise("No enviroment variable POND_PUMP_MODE")
-    "observer" -> :observer
-    "listener" -> :listener
-    _ -> raise("Invalid value for POND_PUMP_MODE - allowed values: observer or listener")
+defmodule PondPump.BuildHelper do
+  def extract_mode! do
+    case System.get_env("POND_PUMP_MODE") do
+      nil -> raise("No enviroment variable POND_PUMP_MODE")
+      "observer" -> :observer
+      "listener" -> :listener
+      _ -> raise("Invalid value for POND_PUMP_MODE - allowed values: observer or listener")
+    end
   end
 end
 
@@ -16,4 +18,4 @@ config :pond_pump,
   receive_pin: 27,
   # General
   power_in_pin: 18,
-  mode: extract_mode!()
+  mode: PondPump.BuildHelper.extract_mode!()
