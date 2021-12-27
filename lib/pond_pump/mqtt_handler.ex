e0aed3b79574e838d5ba8dc116207bab0689b12d
corka149 <corka149@mailbox.org>
2021-12-27 23:37:34 +0100

    Update handler

--- lib/pond_pump/mqtt_handler.ex
+++ lib/pond_pump/mqtt_handler.ex
@@ -3,61 +3,68 @@ defmodule PondPump.MqttHandler do
 
   use Tortoise311.Handler
 
+  require Logger
+
   defstruct []
   alias __MODULE__, as: State
 
+  @topic "pondpump/149"
+
   def init(_opts) do
-    IO.puts("Initializing handler")
+    Logger.info("Initializing handler")
     {:ok, %State{}}
   end
 
   def connection(:up, state) do
-    IO.puts("Connection has been established")
+    Logger.info("Connection has been established")
 
     next_actions = [
-      {:subscribe, "foo/bar", qos: 0}
+      {:subscribe, @topic, qos: 0},
+      {:subscribe, @topic, qos: 1}
     ]
 
     {:ok, state, next_actions}
   end
 
   def connection(:down, state) do
-    IO.puts("Connection has been dropped")
+    Logger.info("Connection has been dropped")
     {:ok, state}
   end
 
   def connection(:terminating, state) do
-    IO.puts("Connection is terminating")
+    Logger.info("Connection is terminating")
     {:ok, state}
   end
 
   def subscription(:up, topic, state) do
-    IO.puts("Subscribed to #{topic}")
+    Logger.info("Subscribed to #{topic}")
     {:ok, state}
   end
 
   def subscription({:warn, [requested: req, accepted: qos]}, topic, state) do
-    IO.puts("Subscribed to #{topic}; requested #{req} but got accepted with QoS #{qos}")
+    Logger.warn("Subscribed to #{topic}; requested #{req} but got accepted with QoS #{qos}")
     {:ok, state}
   end
 
   def subscription({:error, reason}, topic, state) do
-    IO.puts("Error subscribing to #{topic}; #{inspect(reason)}")
+    Logger.error("Error subscribing to #{topic}; #{inspect(reason)}")
     {:ok, state}
   end
 
   def subscription(:down, topic, state) do
-    IO.puts("Unsubscribed from #{topic}")
+    Logger.info("Unsubscribed from #{topic}")
     {:ok, state}
   end
 
   def handle_message(topic, publish, state) do
+    Logger.info("#{Enum.join(topic, "/")} #{inspect(publish)}")
     IO.puts("#{Enum.join(topic, "/")} #{inspect(publish)}")
+
     {:ok, state}
   end
 
   def terminate(reason, _state) do
-    IO.puts("Client has been terminated with reason: #{inspect(reason)}")
+    Logger.warn("Client has been terminated with reason: #{inspect(reason)}")
     :ok
   end
 end
