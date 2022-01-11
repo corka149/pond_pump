3e4539e42998eaa80e886ebb57c1ccadcb37dc19
corka149 <corka149@mailbox.org>
2022-01-11 08:17:08 +0100

    Change extract_mode

--- config/rpi0.exs
+++ config/rpi0.exs
@@ -1,12 +1,21 @@
 import Config
 
+extract_mode! = fn ->
+  case System.get_env("POND_PUMP_MODE") do
+    nil -> raise("No enviroment variable POND_PUMP_MODE")
+    "observer" -> :observer
+    "listener" -> :listener
+    _ -> raise("Invalid value for POND_PUMP_MODE - allowed values: observer or listener")
+  end
+end
+
 config :pond_pump,
   # Observer
   power_in_pin: 18,
   # Listener
   light_pin: 26,
   # General
-  mode: PondPump.BuildHelper.extract_mode!(),
+  mode: extract_mode!.(),
   topic: "pondpump/149",
   mqtt_host: System.fetch_env!("MQTT_HOST"),
   mqtt_port: System.fetch_env!("MQTT_PORT") |> String.to_integer(),
