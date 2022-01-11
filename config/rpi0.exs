f7d366f0ca98954beead09864698c6f923bc814c
corka149 <corka149@mailbox.org>
2022-01-11 07:21:43 +0100

    Move BuildHelper to own modul

--- config/rpi0.exs
+++ config/rpi0.exs
@@ -1,16 +1,5 @@
 import Config
 
-defmodule PondPump.BuildHelper do
-  def extract_mode! do
-    case System.get_env("POND_PUMP_MODE") do
-      nil -> raise("No enviroment variable POND_PUMP_MODE")
-      "observer" -> :observer
-      "listener" -> :listener
-      _ -> raise("Invalid value for POND_PUMP_MODE - allowed values: observer or listener")
-    end
-  end
-end
-
 config :pond_pump,
   # Observer
   power_in_pin: 18,
